247baa4abf0976b1efceb2edaa411cf84faa282c
OruamC <oru_catani@hotmail.com>
2020-12-01 00:02:54 -0300

    Add service annotation

--- src/main/java/one/digitalinnovation/personaapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personaapi/service/PersonService.java
@@ -1,11 +1,13 @@
 package one.digitalinnovation.personaapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import one.digitalinnovation.personaapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personaapi.entity.Person;
 import one.digitalinnovation.personaapi.repository.PersonRepository;
 
+@Service
 public class PersonService {
 	
 	private PersonRepository personRepository;
